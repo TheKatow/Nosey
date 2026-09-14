c2b382f0e653d5040997300034918ff7e15c4694
Flow <91892431+TheKatow@users.noreply.github.com>
2026-09-14 15:30:41 +0000

    Refactor code structure for improved readability and maintainability

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ const STATIC_ASSETS = [
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 // Installation du Service Worker et mise en cache du shell statique
